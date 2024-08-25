ce6dec710bb44f8f82ddd60f28ddc37ff6462a3a
Guilherme Cesar Tomiasi <gtomiasi@gmail.com>
2024-08-24 22:35:42 -0300

    fixing echo && adding zoxide

--- executable_echo_random_quote.sh
+++ executable_echo_random_quote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LINES=$(wc -l ./quotes | awk '{print $1}')
+LINES=$(wc -l $HOME/quotes | awk '{print $1}')
 PRINTED=$(shuf -i 1-$LINES -n 1)
-QUOTE=$(sed -n "${PRINTED}p" ./quotes)
+QUOTE=$(sed -n "${PRINTED}p" $HOME/quotes)
 echo -e "\033[91m$QUOTE\033[0m"
